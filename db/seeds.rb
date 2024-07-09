83875cf8fa5ea611e0e11e5bca8d9aa5879cfcb9
narumi katayama <narumikataiama@icloud.com>
2024-07-09 21:13:50 +0900

    ...

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,15 +45,15 @@ puts 'Creating lists...'
 list_names = ["Action", "Comedy", "Drama", "Horror", "Sci-Fi", "Documentary", "Fantasy", "Classic", "Super Hero"]
 
 list_images = {
-  "Action" => "https://img.freepik.com/fotos-gratis/paisagem-de-guerra-e-conflito-com-soldados-atirando_23-2149766342.jpg?t=st=1720323151~exp=1720326751~hmac=825ca6e7abcf97cfe9b4633e552c3daa8863f1f20b06a9298a9842889c7a8786&w=2000",
-  "Comedy" => "https://img.freepik.com/fotos-gratis/amigos-assistindo-filme-no-cinema_23-2147803785.jpg?t=st=1720323226~exp=1720326826~hmac=0eafa514689c869ab288ded17c97d7a9cb9009a8b1324adee10fe113bfb2a837&w=2000",
-  "Drama" => "https://img.freepik.com/fotos-gratis/encantadora-mulher-atraente-andando-com-jovem-na-rua_23-2148012592.jpg?t=st=1720323308~exp=1720326908~hmac=2ec38ae86481269534cdcf340b2763f93fe5a52b01a0a2f9c1d6d2beb8e6122b&w=2000",
-  "Horror" => "https://img.freepik.com/fotos-gratis/retrato-de-palhaco-assustador_23-2150549660.jpg?t=st=1720323369~exp=1720326969~hmac=af4160972ab9f1ec02af489697bbd3f82634a23f1fb54ffceeb5e18e380b1c5b&w=2000",
-  "Sci-Fi" => "https://img.freepik.com/fotos-gratis/desenho-de-colagem-de-viagem-espacial_23-2150163734.jpg?t=st=1720323409~exp=1720327009~hmac=d37106ea97712dd7830252a5bbee6a12e3e4cebc4df971ebb82e16674a7a6d0e&w=2000",
-  "Documentary" => "https://img.freepik.com/fotos-gratis/listras-negativas-com-claquete-bobinas-de-filme-bilhetes-e-pipoca-na-mesa-de-madeira_23-2148188185.jpg?t=st=1720323437~exp=1720327037~hmac=d5c04f742fde958d4911f0d010d4a78fee3914c2b043c557807a3989bc4b29a5&w=2000",
-  "Fantasy" => "https://img.freepik.com/fotos-gratis/garota-de-vista-traseira-sentada-na-cadeira-no-trem_23-2149666784.jpg?t=st=1720323493~exp=1720327093~hmac=c94920c2d6b9641800ac184e754b628e9f2c7a4e024c426d7e2f7f4448bbdc1e&w=1800",
-  "Classic" => "https://img.freepik.com/fotos-gratis/composicao-de-objetos-de-cinema-close-up_23-2148457830.jpg?t=st=1720323547~exp=1720327147~hmac=20a2740cc53f0cb25ece0454adfa878744a7801ff775e133aad19f4aec4bcb27&w=2000",
-  "Super Hero" => "https://img.freepik.com/fotos-gratis/crianca-na-escola-de-magia-aprendendo-feiticos_23-2150170072.jpg?t=st=1720323395~exp=1720326995~hmac=784ddec9b86f44820396840edf1d1217ab806b0e5f03efafa26d5e3173bd56a0&w=2000"
+  "Action" => "https://images.unsplash.com/photo-1519021228607-ef6e4c22d821?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "Comedy" => "https://images.unsplash.com/photo-1527224857830-43a7acc85260?q=80&w=3871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "Drama" => "https://images.unsplash.com/photo-1611673982064-7385a5d9574e?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "Horror" => "https://images.unsplash.com/photo-1509248961158-e54f6934749c?q=80&w=3837&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "Sci-Fi" => "https://plus.unsplash.com/premium_photo-1682124758854-e6e372888b85?q=80&w=3600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "Documentary" => "https://images.unsplash.com/photo-1611784728558-6c7d9b409cdf?q=80&w=2792&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "Fantasy" => "https://images.unsplash.com/photo-1432958576632-8a39f6b97dc7?q=80&w=3873&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "Classic" => "https://images.unsplash.com/photo-1440404653325-ab127d49abc1?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "Super Hero" => "https://images.unsplash.com/photo-1501432377862-3d0432b87a14?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 }
 
 list_images.each do |list_name, image_url|
