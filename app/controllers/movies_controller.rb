5e20afd93a6801aecef7a426e9a70f4e59cdfa78
narumi katayama <narumikataiama@icloud.com>
2024-07-09 14:28:18 +0900

    movies show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,10 @@ class MoviesController < ApplicationController
 
   def show
     @movie = Movie.find(params[:id])
+    @lists = @movie.lists
+    @available_lists = List.where.not(id: @movie.lists.pluck(:id))
+    @bookmark = Bookmark.new
+    
   end
 
   private
